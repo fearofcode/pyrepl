abc2c647c57b5a1948e73e9a6b5810e8e4463d94
Warren Henning <warren.henning@gmail.com>
2019-06-04 20:57:22 -0700

    use LTO when building

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-gcc -g -O3 -m64 -std=c99 -Wall -Wextra -Wpedantic \
+gcc -g -O3 -m64 -std=c99 -flto -Wall -Wextra -Wpedantic \
     pyrepl.c -o pyrepl \
     $(python3-config --includes) \
     $(python3-config --ldflags)
