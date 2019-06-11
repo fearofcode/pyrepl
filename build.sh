7b3e61fd8b61dca11aca55c9767650cd9cc3a82d
Warren Henning <warren.henning@gmail.com>
2019-06-11 12:34:31 -0700

    Sigh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-gcc -g -O3 -m64 -std=c99 -flto -Wall -Wextra -Wpedantic \
-    pyrepl.c -o pyrepl \
-    $(python3-config --includes) \
-    $(python3-config --ldflags)
--- pyrepl.c
+++ pyrepl.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/inotify.h>
 #include <limits.h>
 #include <unistd.h>
